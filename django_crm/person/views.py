8d3a761cd43ae120c257053145192330041dc7d9
superaidev0410 <super.ai.dev@outlook.com>
2022-08-25 16:48:54 -0700

    add add viewsets of PersonalInfo model

--- django_crm/person/views.py
+++ django_crm/person/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from person.serializers import PersonalInfoSerializer
+from person.models import PersonalInfo
 
 # Create your views here.
+class PersonalInfoViewsets(viewsets.ModelViewSet):
+    """
+    Viewsets of PersonalInfo model
+    """
+    
+    queryset = PersonalInfo.objects.all()
+    serializer_class = PersonalInfoSerializer
