d49efa0bb110cbd580f366f125c829981a19f495
superaidev0410 <super.ai.dev@outlook.com>
2022-08-25 17:27:58 -0700

    add rest api

--- django_crm/django_crm/urls.py
+++ django_crm/django_crm/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('info/', include('person.urls'))
+    path('personal/', include('person.urls'))
 ]
